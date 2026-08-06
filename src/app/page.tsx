3e6ab016ff1af6e9364e7211052932006f925fe4
Uriel1134 <lissanaurioluriel@gmail.com>
2026-08-06 10:58:17 +0100

    image hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -71,7 +71,7 @@ export default function Home() {
 
           <div className="hero-visual" style={{ position: "relative", width: "100%", display: "flex", alignItems: "center", justifyContent: "center" }}>
             <img 
-              src="/hero-main.png?v=3" 
+              src="/hero-main.png" 
               alt="AE PRINT Services Illustration" 
               className="hero-illustration"
             />
